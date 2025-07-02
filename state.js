5a56080b5ebe38da91fbc64c58ad453c0f928945
mklekm <mklekm12@gmail.com>
2025-07-02 16:30:25 +0100

    Update state.js

--- state.js
+++ state.js
@@ -1,17 +1,9 @@
 // state.js
-// هذا الملف يحتفظ بحالة التطبيق الحالية التي تحتاجها الوحدات الأخرى
 export const state = {
     currentUID: null,
     currentStudentId: null,
     currentClassName: null,
-    deferredPrompt: null, // For PWA install prompt
-    charts: { // To hold chart instances
-        classAverageChart: null,
-        overallScoreDistChart: null,
-        componentRadarChart: null,
-        overallComponentRadarChart: null,
-        componentBarChart: null, // This is now obsolete but kept for safety
-        classScoreDistChart: null,
-        componentAnalysisChart: null, // The new chart instance
-    }
+    tempRegData: null, // <-- هذا هو السطر الجديد
+    deferredPrompt: null,
+    charts: {}
 };
